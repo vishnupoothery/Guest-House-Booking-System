788dd20be790edbd1658ed59de0ddd462b7dfecb
jyothsnashaji <jyothsnashaji99@gmail.com>
2019-08-10 14:43:34 +0530

    initial

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/mystyles.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+
+    <title>NITC GH</title>
+  </head>
+  <body>
+    <header class="page-header" id="header">
+      <div class="container-fluid" style="padding-top: 30px">
+
+          <div class="row">
+    <div class="col-sm-6">
+     <img  src="images/logo.jpg">
+    </div>
+    <div class="col-sm-6" >
+      <h3 style="color:#23aacc; float:right;">GUEST HOUSE BOOKING PORTAL</h3>
+    </div>
+  </div>
+
+           </div>
+      </header>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+
+
+
+      <div id="main">
+       <div id="myCarousel" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner">
+    <div class="item active">
+      <img src="images/nitc1.jpeg" style="height:400px ;width: 100%" alt="NITC">
+    </div>
+
+    <div class="item">
+      <img src="images/nitc2.jpeg" style="height:400px ;width: 100%" alt="NITC">
+    </div>
+
+    <div class="item">
+      <img src="images/nitc3.jpeg" style="height:400px ;width: 100%" alt="NITC">
+    </div>
+  </div>
+
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+  <!--
+      <div align="center">
+       <form class="form-inline" action="a.php">
+  <div class="form-group">
+    <label for="checkin">Check-in</label>
+    <input type="date" class="form-control" id="checkin">
+  </div>
+  <div class="form-group">
+    <label for="checkout">Check-out</label>
+    <input type="date" class="form-control" id="checkout">
+  </div>
+<div class="form-group">
+    <label for="number">Number of Rooms</label>
+    <input type="number" class="form-control" id="number">
+  </div>
+
+  <div class="button_cont" style="padding-top: 20px" ><button type="submit" class="example_f" ><span>Check availabiilty</span></button></div>
+</form>
+      </div> -->
+
+
+     <div class=".container-fluid">
+         <div style=" margin-top:25px;">
+         <a class="example_f" href="a.php" >
+             <span align="center" style="width: 90%;">CHECK AVAILABILITY</span>
+         </a>
+         </div>
+
+    </div>
+
+          </div>
+
+  </body>
+</html>
+
